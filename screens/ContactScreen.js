606e8c225ba92a9084064b53661279bcf6e83f1b
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-03-25 11:42:28 -0700

    ContactScreen

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
-
-
+import { Card, Text } from 'react-native-elements';
 
 const ContactScreen = () => {
   return (
-    <ScrollView></ScrollView>
+    <ScrollView>
+        <Card wrapperStyle={{margin: 20}}>
+            <Card.Title>Contact Information</Card.Title>
+            <Card.Divider />
+            <Text>1 Nucamp Way</Text>
+            <Text>Seattle, WA 98001</Text>
+            <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+            <Text>Phone: 1-206-555-1234</Text>
+            <Text>Email: campsites@nucamp.co</Text>
+        </Card>
+    </ScrollView>
   );
 };
 
