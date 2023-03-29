fc54b1103df2adc1152606aebc960b2daad05d7d
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-03-28 20:20:32 -0700

    display comments

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
+import { Avatar, Card, ListItem } from 'react-native-elements';
+import { PARTNERS } from '../shared/partners';
+import { Text } from 'react-native-elements';
+
+export const Mission = () => {
+    return (
+        <Card>
+            <Card.Title>Our Mission</Card.Title>
+            <Card.Divider />
+            <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
+        </Card>
+    );
+};
 
 const AboutScreen = () => {
-  return (
-    <ScrollView></ScrollView>
-  );
+    const [partners, setPartners] = useState(PARTNERS);
+
+    return (
+        <ScrollView>
+            <Mission />
+            <Card>
+                {partners.map((partner) => {
+                    return (
+                        <ListItem key={partner.id}>
+                            <Avatar rounded source={partner.image} />
+                            <ListItem.Content>
+                                <ListItem.Title>{partner.name}</ListItem.Title>
+                                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+                            </ListItem.Content>
+                        </ListItem>
+                    )
+                })}
+            </Card>
+        </ScrollView>
+    );
 };
 
 export default AboutScreen;
--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,10 +1,56 @@
 import RenderCampsite from "../features/campsites/RenderCampsite";
+import { useState } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { COMMENTS } from "../shared/comments";
 
 const CampsiteInfoScreen = ({ route }) => {
+
     const { campsite } = route.params;
+    const [comments, setComments] = useState(COMMENTS);
+    const renderCommentItem = ({ item }) => {
+        return (
+            <View style={styles.commentItem}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating}</Text>
+                <Text style={{ fontSize: 12 }}>{`--${item.author}, ${item.date}`}</Text>
+            </View>
+        )
+    }
+
     return (
-        <RenderCampsite campsite={campsite} />
+        <FlatList 
+            data={comments.filter((comment) => comment.campsiteId === campsite.id)}
+            renderItem={renderCommentItem}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{
+                marginHorizontal: 20,
+                paddingVertical: 20,
+            }}
+            ListHeaderComponent={
+                <>
+                    <RenderCampsite campsite={campsite} />
+                    <Text style={styles.commentsTitle}>Comments</Text>
+                </>
+            }
+        />
     );
 };
+
+const styles = StyleSheet.create({
+    commentsTitle: {
+        textAlign: "center",
+        backgroundColor: "#fff",
+        fontSize: 16,
+        fontWeight: "bold",
+        color: "#48434D",
+        padding: 10,
+        paddingTop: 30
+    },
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: "#fff"
+    }
+});
 
 export default CampsiteInfoScreen;
