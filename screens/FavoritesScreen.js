ac128d3874af78d3e3f6e3f466ed72f207ac6913
ECadenceKahn <eleanor.c.kahn@gmail.com>
2023-04-05 19:54:26 -0700

    Alerts before deleting a favorite

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,5 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
-import { View, FlatList, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { 
+    View, 
+    FlatList, 
+    Text, 
+    TouchableOpacity, 
+    StyleSheet,
+    Alert
+} from "react-native";
 import { Avatar, ListItem } from "react-native-elements";
 import Loading from "../components/LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
@@ -18,7 +25,21 @@ const FavoritesScreen = ({ navigation }) => {
                 <View style={styles.deleteView}>
                     <TouchableOpacity 
                         style={styles.deleteToucheable} 
-                        onPress={() => dispatch(toggleFavorite(campsite.id))}
+                        onPress={() => Alert.alert("Delete Favorite?", 
+                        "Are you sure you wish to delete the favorite campsite" + campsite.name + "?",
+                        [
+                            {
+                                text: "Cancel",
+                                onPress: () => console.log(campsite.name + "Not deleted"),
+                                style: "cancel"
+                            },
+                            {
+                                text: "OK",
+                                onPress: () => dispatch(toggleFavorite(campsite.id))
+                            }
+                        ],
+                        { cancelable: false }
+                        )}
                     >
                         <Text style={styles.deleteText}>Delete</Text>
                     </TouchableOpacity>
